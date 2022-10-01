fe6a7a450375d22fee4471c9c4ec86761f7e020d
HENG-PI-PI <107341130+HENG-PI-PI@users.noreply.github.com>
2022-10-01 16:24:06 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,4 +2,5 @@ let h1 = document.querySelector('h1')
 let body = document.querySelector('body')
 h1.addEventListener('mouseover',function(){
     body.style.backgroundColor = 'green'
+    console.log("测试文本")
 })
